fadb7df2642215a01060d25ca25f8aa97810dcb4
Simon Rehn <simon.rehn1@icloud.com>
2025-04-19 17:35:38 +0200

    MacOS support and BangPatterns

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-HFLAGS="-XHaskell98 -XNoNPlusKPatterns -Wall -Wextra"
+HFLAGS="-XHaskell98 -XNoNPlusKPatterns -XBangPatterns -Wall -Wextra"
+
+PLATFORM_FLAGS=""
+if [ "$(uname)" = "Darwin" ]; then
+    PLATFORM_FLAGS="-lglfw3 -framework OpenGL -framework Cocoa -framework IOKit"
+else
+    # TODO: Make it so that you specify debug/release as an argument
+    PLATFORM_FLAGS="-lglfw -lGL -DGL_DEBUG"
+fi
 
 cd source/
 set -xe
-ghc $HFLAGS -lglfw -lGL -DGL_DEBUG --make Main.hs -o ../game.exe
+ghc $HFLAGS $PLATFORM_FLAGS --make Main.hs -o ../game.exe
